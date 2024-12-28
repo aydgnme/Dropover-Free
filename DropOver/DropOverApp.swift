4526f6dc5b8ac832fe0d10b695d9308a7a9d8b54
Aydogan Mert <mertaydogn0@gmail.com>
2024-12-28 12:46:14 +0200

    Hello World

---
+++ DropOver/DropOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropOverApp.swift
+//  DropOver
+//
+//  Created by Mert Aydoğan on 28.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DropOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
